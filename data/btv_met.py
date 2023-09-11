57554f73f72763ddc4cc9bb25bc5a34d7c7e3ff9
Patrick J. Clemins <patrick.clemins@uvm.edu>
2023-09-11 16:02:54 -0400

    Update btv_met to remove bad data - Some TCDC records had no data - Some RAIN records came back NaN or had   notations on them that messed with their   conversion to float - Bug fixes

--- data/btv_met.py
+++ data/btv_met.py
@@ -1,17 +1,18 @@
 import requests
 import json
 import pandas as pd
+import numpy as np
 import datetime
 from datetime import date
 
 def splitsky ( instring ) :
-  thestring = str(instring)
-  tokens = thestring.split(':')   # looking for at least one cover code marker :
-  if len(tokens) > 1 :
-    token = tokens[-2].split()[-1]  # keep the cover code just before last : marker
-  else:
-    token = " "     # any records without a : cover code separator end up as a blank
-  return token
+	thestring = str(instring)
+	tokens = thestring.split(':')   # looking for at least one cover code marker :
+	if len(tokens) > 1 :
+		token = tokens[-2].split()[-1]  # keep the cover code just before last : marker
+	else:
+		token = " "     # any records without a : cover code separator end up as a blank
+	return token
 
 def sky2prop (theskycode) :
   skypropmap = {'CLR': 0.000, 'FEW': 0.250, 'SCT': 0.5000, 'BKN': 0.875, 'OVC': 1.000, 'VV': 1.000, ' ': 1.000}
@@ -19,13 +20,16 @@ def sky2prop (theskycode) :
   return theprop
 
 def leavenotrace (precip) :
-  if str(precip) == 'T' :
-    return '0.0'
-  else :
-    return precip
+	if str(precip) == 'T' :
+		return '0.00'
+	else :
+		#return precip
+		return str(precip).replace('s', '')
 
 def create_final_df(df, colToKeep, index):
-    df = pd.DataFrame(data={colToKeep: df[colToKeep]}, index=pd.to_datetime(index))
+	# print(f'Creating final df for {colToKeep}')
+	# print(df[colToKeep])
+	return pd.DataFrame(data={colToKeep: df[colToKeep].to_numpy()}, index=pd.DatetimeIndex(data=pd.to_datetime(df[index]), name='time'))
 
 def retrieve_data(startDate, endDate, variable):
 	requeststring = 'https://www.ncei.noaa.gov/access/services/data/v1/'+\
@@ -36,15 +40,15 @@ def retrieve_data(startDate, endDate, variable):
 							'&endDate='+\
 								str(endDate)+\
 							'&dataTypes='+\
-                                variable+\
+								variable+\
 							'&format=json' 
 	print(requeststring)
 	result = requests.get(requeststring)
-    
-	print(result.text)
+	
+	# print(result.text)
 
 	return pd.DataFrame(result.json())
-    
+	
 
 def get_data () :
 
@@ -65,21 +69,28 @@ def get_data () :
 		# result = requests.get(requeststring)
 
 		# df = pd.DataFrame(result.json())
-        
+		
 		cloud_df = retrieve_data(startday, endday, 'HourlySkyConditions')
 		precip_df = retrieve_data(startday, endday, 'HourlyPrecipitation')
-        
-		print(cloud_df)
-		print(precip_df)
-        
+		
+		# print(cloud_df)
+		# print(precip_df)
+		
 		returnDict = {}
 
 		cloud_df['skycode'] = cloud_df['HourlySkyConditions'].apply(splitsky)
-		cloud_df['TCDC'] = cloud_df['skycode'].apply(sky2prop)
-        
-		precip_df['RAIN'] = precip_df['HourlyPrecipitation'].apply(leavenotrace)
+		cloud_df['TCDC'] = cloud_df['skycode'].apply(sky2prop).astype('float')
+		# Remove those that don't convert to skycode... junk entries
+		cloud_df = cloud_df[cloud_df['skycode'] != ' ']
+		
+		# First replace 'T's for trace precip with 0.0
+		#  leavenotrace also removes 's' notations on some precip values
+		#  Also, convert to float
+		precip_df['RAIN'] = precip_df['HourlyPrecipitation'].apply(leavenotrace).astype('float')
+		# Then, dump rows with NaN for RAIN
+		precip_df = precip_df[~precip_df['RAIN'].isna()]
 
 		returnDict['TCDC'] = create_final_df(cloud_df, 'TCDC', 'DATE')
 		returnDict['RAIN'] = create_final_df(precip_df, 'RAIN', 'DATE')
 
-		return dict
+		return returnDict
