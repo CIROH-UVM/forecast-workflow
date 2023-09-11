338c3e485ab2bfe782d66995ddcabc2249f8c415
Patrick J. Clemins <patrick.clemins@uvm.edu>
2023-09-11 12:48:33 -0400

    Updates to colchester_reef_met.py - Rename 'T2C' to the correct 'T2' - Add parse_dates=True to csv parsing to   get a datetimeIndex for the returned DataFrame

--- data/colchester_reef_met.py
+++ data/colchester_reef_met.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 def get_data():
     with open(os.path.join("/data/forecastData/colchesterReefFEMC", "Z0080_CR_QAQC.csv")) as file:
-        cr_data = pd.read_csv(file, delimiter=",", header=0, index_col=0) # Make DateTime as index
+        cr_data = pd.read_csv(file, delimiter=",", header=0, index_col=0, parse_dates=True) # Make DateTime as index
         cr_df = pd.DataFrame(cr_data)
         # Keep the last 90 days (24h*4quarters*90days=8640) plus buffer
         cr_df = cr_df.tail(8800)
         # Subset to the columns of interest
         cr_df = cr_df[['38m_AIRTEMP','PYRANOM','38m_RELHUMID', 'NRG_38m_MEAN_RESULTANT_WINDSPEED','NRG_38m_MEAN_WIND_DIRECTION']]
-        cr_df.columns = ['T2C', 'SWDOWN', 'RH2', 'WSPEED', 'WDIR']
+        cr_df.columns = ['T2', 'SWDOWN', 'RH2', 'WSPEED', 'WDIR']
     return cr_df
