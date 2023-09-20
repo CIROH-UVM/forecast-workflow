247ca335b3ea7c83d55a22393feb1ecbeaf3fd8c
Scott E Turnbull <seturnbu@ciroh-testbed.uvm.edu>
2023-09-20 09:35:49 -0400

    Combine URL of recent data with long term data

--- data/colchester_reef_met.py
+++ data/colchester_reef_met.py
@@ -5,8 +5,20 @@ import datetime as dt
 
 def get_data(ForecastStartDate, SpinupStartDate):
     with open(os.path.join("/data/forecastData/colchesterReefFEMC", "Z0080_CR_QAQC.csv")) as file:
-        cr_data = pd.read_csv(file, delimiter=",", header=0, index_col=0, parse_dates=True) # Make DateTime as index
-        cr_df = pd.DataFrame(cr_data)
+        hcr_data = pd.read_csv(file, delimiter=",", header=0, index_col=0, parse_dates=True) # Make DateTime as index
+        hcr_df = pd.DataFrame(hcr_data)
+
+        # Also get the URL for the most recent Colchester Reef data in the CSV. Concat the two frames.
+        url="https://uvm.edu/femc/MetData/ColReefQAQC/CR_QAQC_latest.csv"
+        scr_df = pd.DataFrame(pd.read_csv(url, delimiter=",", header=0, index_col=0, parse_dates=True))
+
+        #print(scr_df)
+
+        #cr_df = pd.concat([hcr_df, scr_df], axis=0, ignore_index=True)   # ignore dupe time indexes (overlap)
+        cr_df = pd.concat([hcr_df, scr_df], axis=0) 
+
+        #print(cr_df)
+
         # Before: Keep the last 90 days (24h*4quarters*90days=8640) plus buffer
         # cr_df = cr_df.tail(8800)
         # Now: Keep the days from SpinupStartDate to present
