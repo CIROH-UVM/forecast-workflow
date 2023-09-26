2cda0f0cec53d19a639850c6c304917b0bc0a9a5
Patrick J. Clemins <patrick.clemins@uvm.edu>
2023-09-26 19:44:00 -0400

    Refactor and update archive_fcns - Make functions for:   archive_directories()   remove_directories() - Rewrite other functions to use above functions - Change references to folders to directories - Add source directory to individual directories - Begin conversation from .format() to f'' - Other cosmetic changes

--- data/archive_fcns.py
+++ data/archive_fcns.py
@@ -3,6 +3,20 @@ from glob import glob
 import os
 import sh
 
+def remove_directories(dirs):
+	for dir in dirs:
+		print('Deleting {}'.format(dir))
+		#sh.rm('-rf', dir)
+		print('Successfully deleted {}'.format(dir))
+
+
+def archive_directories(source_dirs, destination_dir):
+	for source_dir in source_dirs:
+		print('Archiving {}'.format(source_dir))
+		sh.rsync('-a', source_dir, destination_dir)
+		print('Successfully archived {} in {}'.format(source_dir, destination_dir))
+
+
 ### Moves all forecast data except those from the past n days to a specified archive directory
 # -- source (str) [required]: absolute path to the folder in which the forecast data are located
 # -- destination (str) [required]: absolute path to the folder in which the forecast data will be archived
@@ -18,15 +32,21 @@ def archive_forecasts(source = "/data/forecastRuns/", destination = "/netfiles/c
 	# all_dates = glob('7dayforecast-*')
 	# to_archive = [date for date in all_dates if date not in past5]
 	#### New file list method
-	to_archive = [file for file in os.listdir(source) if file not in past5]
+	to_archive = [dir for dir in os.listdir(source) if dir not in past5]
 	
-	print('the following folders in {} will be moved to {}:\n{}'.format(source, destination, to_archive))
-	for folder in to_archive:
-		print('archiving {}'.format(folder))
-		sh.rsync(folder, destination)
-		print('successfully archived {} in {}'.format(folder, destination))
+	#print('The following folders in {} will be archived to {}:\n{}'.format(source, destination, to_archive))
+	print(f'The following folders in {source} will be archived to {destination}:\n{to_archive}')
+	archive_directories([os.path.join(source, directory) for directory in to_archive], destination)
+	# for folder in to_archive:
+	# 	print('Archiving {}'.format(os.path.join(source, folder)))
+	# 	#sh.rsync('-a', os.path.join(source, folder), destination)
+	# 	print('Successfully archived {} in {}'.format(os.path.join(source, folder), destination))
 	print('Forecast archive process complete')
-	return
+
+	print('The following directories will be deleted:\n{}'.format(to_archive))
+	remove_directories([os.path.join(source, directory) for directory in to_archive])
+	print('Forecast directory deleting complete')
+
 
 ### Moves all gfs data except those from the past n days to a specified archive directory
 # -- source (str) [required]: absolute path to the folder in which the gfs data are located
@@ -44,15 +64,20 @@ def archive_gfs(source, destination, past_n = 5):
 	# all_dates = glob('gfs.*')
 	# to_archive = [date for date in all_dates if date not in past5]
 	#### New file list method
-	to_archive = [file for file in os.listdir(source) if file not in past5]
+	to_archive = [dir for dir in os.listdir(source) if dir not in past5]
 
-	print('the following folders in {} will be moved to {}:\n{}'.format(source, destination, to_archive))
-	for folder in to_archive:
-		print('archiving {}'.format(folder))
-		sh.rsync(folder, destination)
-		print('successfully archived {} in {}'.format(folder, destination))
+	print('The following folders in {} will be archived to {}:\n{}'.format(source, destination, to_archive))
+	archive_directories([os.path.join(source, directory) for directory in to_archive], destination)
+	# for folder in to_archive:
+	# 	print('Archiving {}'.format(os.path.join(source, folder)))
+	# 	#sh.rsync('-a', os.path.join(source, folder), destination)
+	# 	print('Successfully archived {} in {}'.format(os.path.join(source, folder), destination))
 	print('GFS archive process complete')
-	return
+
+	print('The following directories will be deleted:\n{}'.format(to_archive))
+	remove_directories([os.path.join(source, directory) for directory in to_archive])
+	print('GFS directory deleting complete')
+
 
 ### Moves all nwm data except those from the past n days to a specified archive directory
 # -- source (str) [required]: absolute path to the folder in which the nwm data are located
@@ -69,12 +94,16 @@ def archive_nwm(source, destination, past_n = 5):
 	# all_dates = glob(today.strftime('%Y')+'*')
 	# to_archive = [date for date in all_dates if date not in past5]
 	#### New file list method
-	to_archive = [file for file in os.listdir(source) if file not in past5]
+	to_archive = [dir for dir in os.listdir(source) if dir not in past5]
 
-	print('the following folders in {} will be moved to {}:\n{}'.format(source, destination, to_archive))
-	for folder in to_archive:
-		print('archiving {}'.format(folder))
-		#sh.rsync(folder, destination)
-		print('successfully archived {} in {}'.format(folder, destination))
+	print('The following folders in {} will be archived to {}:\n{}'.format(source, destination, to_archive))
+	archive_directories([os.path.join(source, directory) for directory in to_archive], destination)
+	# for folder in to_archive:
+	# 	print('Archiving {}'.format(os.path.join(source, folder)))
+	# 	#sh.rsync('-a', os.path.join(source, folder), destination)
+	# 	print('successfully archived {} in {}'.format(os.path.join(source, folder), destination))
 	print('NWM archive process complete')
-	return
+
+	print('The following directories will be deleted:\n{}'.format(to_archive))
+	remove_directories([os.path.join(source, directory) for directory in to_archive])
+	print('NWM directory deleting complete')
