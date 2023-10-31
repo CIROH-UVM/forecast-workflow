1feab04ef5ffc5dff668b39d1f656c80137018eb
Noah Beckage <nbeckage@uvm.edu>
2023-10-31 19:05:02 -0400

    create default args, start parsing in AEM3D.py

--- models/aem3d/AEM3D.py
+++ models/aem3d/AEM3D.py
@@ -1,3 +1,6 @@
+from default_config import defaults
+import argparse
+import configparser
 
 ## Check on naming convention for classes and functions and variables
 class Model:
@@ -11,14 +14,15 @@ class Model:
     
     # Copy base files
     # Mod setup
-    generate_input_files()
+    # generate_input_files()
+    pass
 
   def generate_input_files():
     # Use starttime / endtime from model creation
-
+    
     #generate_temp()
     # and so on... See current AEM3D
-
+    pass
 
   def input_from_calibration_file(filename, newRange, origRange = None):
     #If origRange = None, start at first mm/dd same as newRange
@@ -30,18 +34,19 @@ class Model:
     
     #Create Pandas dataframe from lists within list
     #return dataframe
+    pass
   
   def input_from_NWS_v21_hindcast():
     #read in hindcast to dataFrame
+    pass
 
 class InputFile:
-  def write(headers = {}, data):
+  def write(data, headers = {}):
+    pass
   
   def read():
     # Return {{Headers}, pandas DF}
-
-
-
+    pass
 
 
 ## Emulate Python datetime... just add conversion capabilities to/from AEM3D datetime
@@ -52,21 +57,34 @@ class Datetime:
   aem3d_datetime = None  ## Or, just format on the fly each time
 
   def from_AEM3D_datetime(aem3d_datetime):
+    pass
   
   def to_AEM3D_datetime():
-    return aem3d_datetime
+    # return aem3d_datetime
+    pass
 
   def to_datetime():
-    return datetime
+    # return datetime
+    pass
+
+def parse_config():
+  pass
+
+def parse_args():
+  pass
   
 def main():
-  settings = parse_args(sys.argv[1:])
+  # settings = parse_args(sys.argv[1:])
+  pass
 
   #Create model
 
+def test():
+  print('hello world')
 
 if __name__ == '__main__':
-    main()
+    # main()
+    test()
 
 
 
