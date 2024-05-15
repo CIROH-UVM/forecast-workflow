68a966dae14a674ad9923030701ea690906abcc5
Noah Beckage <nbeckage@uvm.edu>
2024-05-15 14:15:35 -0400

    Enhancements for lcd_ob.get_data(): - add HourlyDryBulbTemperature as valid variable to get - process air temp to remove special indicator characters - add units to series name - implement user-defined var names in final series - remove duplicate timestamps from raw data

--- data/lcd_ob.py
+++ data/lcd_ob.py
@@ -1,7 +1,7 @@
 import datetime as dt
 import requests
 import pandas as pd
-from .utils import parse_to_datetime, logger
+from .utils import parse_to_datetime, fahr_to_celsius
 
 def splitsky ( instring ) :
 	thestring = str(instring)
@@ -20,6 +20,35 @@ def sky2prop (theskycode) :
   theprop = skypropmap[str(theskycode)]
   return theprop
 
+def leavenosuspect(raw_temp):
+	'''
+	Processes air temp Series to strip away 's' and '*' characters from the data. These are special indicator characters appended to some air temp values
+	from the Burlington Airport weather station. For more info on special indicator characters, see https://www.ncei.noaa.gov/pub/data/cdo/documentation/LCD_documentation.pdf
+	
+	Args:
+	-- raw_temp (Pandas Series) [req]: the raw temp Series to parse
+
+	Returns:
+	A copy of the passed series with 's' and '*' characters removed
+	'''
+	print('Removing special indicator chars from raw air temp data...')
+	corrected_temp = raw_temp.copy()
+	for i, value in enumerate(raw_temp.astype(str)):
+		corrected_value = ''
+		# correct suspect values
+		if 's' in value:
+			corrected_value = value.replace('s','')
+			corrected_temp[i] = corrected_value
+		# correct missing values
+		if '*' in value:
+			corrected_value = value.replace('*', 'nan')
+			corrected_temp[i] = corrected_value
+		# if no correction is needed, use original value
+		if corrected_value:
+			print(f'Value at index {i} corrected from {value} to {corrected_value}')
+		else: corrected_temp[i] = value
+	return corrected_temp.astype(float)
+
 def leavenotrace (precip) :
 	if str(precip) == 'T' :
 		return '0.00'
@@ -34,25 +63,40 @@ def leavenotrace (precip) :
 def create_final_df(df, colToKeep, index):
 	# print(f'Creating final df for {colToKeep}')
 	# print(df[colToKeep])
-	
+	# print(df['Units'])
 	# 20231211 - set index as datetime with timezone suffix set to UTC; data is in UTC, tz_localize() tells pandas that
-	return pd.DataFrame(data={colToKeep: df[colToKeep].to_numpy()}, index=pd.DatetimeIndex(data=pd.to_datetime(df[index]), name='time')).tz_localize('UTC')
+	return pd.DataFrame(data={colToKeep: df[colToKeep].to_numpy(), 'Units':df['Units'].to_numpy()}, index=pd.DatetimeIndex(data=pd.to_datetime(df[index]), name='time')).tz_localize('UTC')
 
-def process_rain(precip_df):
+def process_rain(precip_df, user_name):
 	# First replace 'T's for trace precip with 0.0
 	#  leavenotrace also removes 's' notations on some precip values
 	#  Also, convert to float
-	precip_df['RAIN'] = precip_df['HourlyPrecipitation'].apply(leavenotrace).astype('float')
+	precip_df[user_name] = precip_df['HourlyPrecipitation'].apply(leavenotrace).astype('float')
 	# Then, dump rows with NaN for RAIN
 	precip_df = precip_df[~precip_df['RAIN'].isna()]
+	# add units - gathered from LCD documentation
+	precip_df = precip_df.assign(Units='inches')
 	return precip_df
 
-def process_clouds(cloud_df):
+def process_clouds(cloud_df, user_name):
 	cloud_df['skycode'] = cloud_df['HourlySkyConditions'].apply(splitsky)
 	# Remove those that don't convert to skycode... junk entries
 	cloud_df = cloud_df[cloud_df['skycode'] != ' ']
-	cloud_df['TCDC'] = cloud_df['skycode'].apply(sky2prop).astype('float')
+	cloud_df[user_name] = cloud_df['skycode'].apply(sky2prop).astype('float')
+	# add units
+	cloud_df = cloud_df.assign(Units='%')
 	return cloud_df
+
+def process_air_temp(temp_df, user_name):
+	# create a new column with user-defined var name, and strip special indicator characters
+	temp_df[user_name] = leavenosuspect(temp_df['HourlyDryBulbTemperature'])
+	# uncomment the below line to convert temperature to celsius
+	# temp_df[user_name] = fahr_to_celsius(temp_df[user_name])
+	# drop NA's, based on corrected column
+	temp_df = temp_df[~temp_df[user_name].isna()]
+	# adding a units column
+	temp_df = temp_df.assign(Units='\N{DEGREE SIGN}F')
+	return temp_df
 
 def retrieve_data(startDate, endDate, variable, station_id):
 	# put this in loop since this fails frequently
@@ -91,39 +135,27 @@ def retrieve_data(startDate, endDate, variable, station_id):
 def get_data(start_date,
 			 end_date,
 			 locations,
-			 variables,
-			 return_type='dict'):
+			 variables):
 	"""
 	A function to download and process NOAA Local Climatological Data data to return nested dictionary of pandas series for each variable, for each location.
 
 	Args:
-	-- start_date (str, date, or datetime) [req]: the start date for which to grab LCD data
-	-- end_date (str, date, or datetime) [req]: the end date for which to grab LCD data
+	-- start_date (str, date, or datetime) [req]: the start date for which to grab LCD data.
+	-- end_date (str, date, or datetime) [req]: the end date for which to grab LCD data.
 	-- locations (dict) [req]: a dictionary (stationID/name:IDValue/latlong tuple) of locations to get USGS data for.
-	-- variables (dict) [req]: a dictionary of variables to download. Values should be AEM3D-specfic variable shortname, key should be LCD-specific variable name. See LCD spreadsheet for full list of available vars.
-	-- return_type (string) [opt]: string indicating which format to return data in. Default is "dict", which will return data in a nested dict format:
-									{locationID1:{
-										var1_name:pd.Series,
-										var2_name:pd.Series,
-										...},
-									locationID2:{...},
-									...
-									}
-									Alternative return type is "dataframe", which smashes all data into a single dataframe muliIndex'd by station ID, then timestamp
-	
+	-- variables (dict) [req]: a dictionary of variables to download, where keys are user-defined variable names and values are LCD-specific variable names.
+								Currently only works for LCD variables HourlyPrecipitation, HourlySkyCondtions, and HourlyDryBulbTemperature. 
+		
 	Returns:
-	NOAA Local Climatological Data (total cloud cover and precipitation currently) for the dat range and locations provided
+	NOAA Local Climatological Data timeseries for the given locations in a nested dict format where 1st-level keys are user-provided location names and 2nd-level keys
+	are variables names and values are the respective data in a Pandas Series object.
 	"""
-	# end_date = date.today()
-	#d = datetime.timedelta(days = 90)
-	#start_date = end_date - d
+
 	# 20231211 - Do not adjust passed dates to a previous day, that's a caller concern if that data buffer is needed
 	start_date = parse_to_datetime(start_date).date()
 	end_date = parse_to_datetime(end_date).date()
 	lcd_data = {loc:{} for loc in locations.keys()}
 
-	# variables = {'HourlySkyConditions':'TCDC',
-	# 		  	 'HourlyPrecipitation':'Rain'}
 	for station, id in locations.items():
 		# define an empty return dict for each station
 		returnDict = {}
@@ -131,47 +163,28 @@ def get_data(start_date,
 		raw_data = {}
 		processed_data = {}
 		# retrieve all of the data in the variables dictionary
-		for lcd_name, short_name in variables.items():
-			raw_data[short_name] = retrieve_data(start_date, end_date, lcd_name, id)
+		for user_name, lcd_name in variables.items():
+			raw_data[user_name] = retrieve_data(start_date, end_date, lcd_name, id)
+			# keep first instance of any duplicates in the raw data
+			print('Removing duplicate timestamps from raw data, keeping first instance of each duplication...')
+			raw_data[user_name] = raw_data[user_name].loc[~raw_data[user_name]['DATE'].duplicated(keep='first')]
 			try:
-				logger.info(f"{lcd_name} for station ID {id}")
-				logger.info(raw_data[short_name])
+				print(f"{lcd_name} for station ID {id}")
+				print(raw_data[user_name])
 			except Exception as e:
 				print(f'{type(e)}:{e}')
 			# add more cases as more variables come in and need specific processing
 			match lcd_name:
 				case 'HourlySkyConditions':
-					processed_data[short_name] = process_clouds(raw_data[short_name])
+					processed_data[user_name] = process_clouds(raw_data[user_name], user_name)
 				case 'HourlyPrecipitation':
-					processed_data[short_name] = process_rain(raw_data[short_name])
-		# try:
-		# 	logger.info('cloud_df in btv_met')
-		# 	logger.info(cloud_df)
-		# 	logger.info('precip_df in btv_met')		
-		# 	logger.info(precip_df)
-		# except Exception as e:
-		# 	print(f'{type(e)}:{e}')
+					processed_data[user_name] = process_rain(raw_data[user_name], user_name)
+				case 'HourlyDryBulbTemperature':
+					processed_data[user_name] = process_air_temp(raw_data[user_name], user_name)
+			
+			returnDict[user_name] = create_final_df(processed_data[user_name], user_name, 'DATE')
+					
+		# created nested dictionary of pd.Series for each variable for each location
+		lcd_data[station] = {var:df[var].rename(f'Units: {df["Units"][0]}') for var, df in returnDict.items()}
 
-		# cloud_df['skycode'] = cloud_df['HourlySkyConditions'].apply(splitsky)
-		# # Remove those that don't convert to skycode... junk entries
-		# cloud_df = cloud_df[cloud_df['skycode'] != ' ']
-		# cloud_df['TCDC'] = cloud_df['skycode'].apply(sky2prop).astype('float')
-		
-		# # First replace 'T's for trace precip with 0.0
-		# #  leavenotrace also removes 's' notations on some precip values
-		# #  Also, convert to float
-		# precip_df['RAIN'] = precip_df['HourlyPrecipitation'].apply(leavenotrace).astype('float')
-		# # Then, dump rows with NaN for RAIN
-		# precip_df = precip_df[~precip_df['RAIN'].isna()]
-
-			returnDict[short_name] = create_final_df(processed_data[short_name], short_name, 'DATE')
-
-		# ensure return_type is a valid value
-		if return_type not in ['dict', 'dataframe']:
-			raise ValueError(f"'{return_type}' is not a valid return_type. Please use 'dict' or 'dataframe'")
-		elif return_type == 'dict':
-			# created nested dictionary of pd.Series for each variable for each location
-			lcd_data[station] = {var:df[var] for var, df in returnDict.items()}
-		elif return_type == 'dataframe':
-			raise Exception("'dataframe' option not implemented yet. Please use return_type = 'dict'")
 	return lcd_data
