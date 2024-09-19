b49a58ac7559e98cc8d428044bf63a6db2c443a6
Noah Beckage <nbeckage@uvm.edu>
2024-09-19 19:59:58 -0400

    launchScenarioTEMPLATE.sh: implement command-line args to increase interoperability

--- batch/launchScenarioTEMPLATE.sh
+++ batch/launchScenarioTEMPLATE.sh
@@ -12,10 +12,15 @@
 
 source /users/n/b/nbeckage/miniconda3/etc/profile.d/conda.sh
 # source /users/p/c/pclemins/usr/local/miniforge3/etc/profile.d/conda.sh
+
 conda activate forecast
 
+# parse cq dir (1st arg) scenario dir (2nd arg)
+CQ_DIR=$1
+SCENARIO_DIR=$2
+
 # change to whatever directory you want to put your scenario runs in
-cd /netfiles/ciroh/7dayHABsHindcast/<FEE_VERSION>/<CQ_PARADIGM>/<SCENARIO_DIR>/
+cd /netfiles/ciroh/7dayHABsHindcast/<FEE_VERSION>/$CQ_DIR/$SCENARIO_DIR/
 
 # note that you must have an experiment-specific configuration file in your scenario directory
-python ../../launchExperiment.py experiment_config.json
+python /users/n/b/nbeckage/ciroh/forecast-workflow/batch/launchExperiment.py experiment_config.json
