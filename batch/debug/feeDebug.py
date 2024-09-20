b3c3610f41112a818de2ce51582f6df7dc7421df
Noah Beckage <nbeckage@uvm.edu>
2024-09-20 19:29:05 -0400

    feeDebug.py: add error parsing for AEM3D_prep_IAM.py failures

--- batch/debug/feeDebug.py
+++ batch/debug/feeDebug.py
@@ -13,7 +13,7 @@ If a failed run is detected, the error is diagnosed and reported. This script cr
 reports error messages detected in failed runs, and (2) a concise csv summary file of failed runs and their causes.
 '''
 
-def aem3d_fatal(fatal_msg):
+def aem3d_fatal(fatal_msg, err_report):
 	'''
     Function that parses error messages from AEM3D model runs, identifying fatal errors and extracting relevant details.
 
@@ -28,7 +28,7 @@ def aem3d_fatal(fatal_msg):
         - 'last_date_ord' (str or None): The last date in ordinal form before the error occurred (if applicable).
         - 'last_date_dt' (str or None): The last date in human-readable datetime format before the error occurred (if applicable).
     '''
-	err_report = {'error':'aem3d_fatal', 'err_type':None, 'file':None, 'last_date_ord':None, 'last_date_dt':None}
+	err_report['error'] = 'aem3d_fatal'
 	for i, line in enumerate(fatal_msg):
 		print(f'\t\t\t\t {line}')
 		# add more if clauses to deal with other AEM3D fatal error types as they come up
@@ -66,7 +66,16 @@ def get_timestamps(ds):
 		timestamps.append(ts)
 	return timestamps
 
-def parse_failed_run(filelines):
+def prep_IAM_err(prep_err, err_report):
+	err_report['error'] = 'prep_IAM'
+	prnt = False
+	for i, line in enumerate(prep_err):
+		if "During handling of the above exception" in line: prnt = True
+		if prnt: print(f'\t\t\t\t {line}')
+		if "AEM3D_prep_worker - None" in line: prnt = False
+	return err_report
+
+def parse_failed_run(filelines, report):
 	'''
     Function that parses the lines of the .out file to detect what kind of error caused the run to fail.
 	Passes on the .out file lines to more specific error parser functions once a braod kind of error is determined
@@ -78,12 +87,19 @@ def parse_failed_run(filelines):
     -- report (dict or None): A dictionary containing the parsed fatal error report if a fatal error is found, or None
     	if no fatal error is detected. The structure of the report is the same as the output of the aem3d_fatal() function.
     '''
+	# for line in filelines:
+	# 	print(line)
 	for i, line in enumerate(filelines):
 		# check if AEM3D threw a fatal error
 		if "***** FATAL ERROR ****" in line:
 			fatal_error = [line.rstrip(' \n') for line in filelines[i:i+9] if line != '\n']
-			report = aem3d_fatal(fatal_error)
-			return report
+			report = aem3d_fatal(fatal_error, report)
+			break
+		if "AEM3D_prep_IAM.py failed." in line:
+			prep_error = [line.rstrip(' \n') for line in filelines[i:] if line != '\n']
+			report = prep_IAM_err(prep_error, report)
+			break
+	return report
 
 def report_df(failure_dict):
 	'''
@@ -134,7 +150,7 @@ file_path = "*.out"
 # NOTE: last 150 lines should
 last_lines_to_read = 150
 # provide a custom log name
-log_name = 'rf2023BlM3.log'
+log_name = 'rf2023BLM3'
 
 # Configure logging
 logging.basicConfig(
@@ -143,7 +159,7 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger()
-logger.addHandler(logging.FileHandler(log_name, 'w'))
+logger.addHandler(logging.FileHandler(f'{log_name}.log', 'w'))
 print = logger.info
 
 def main():
@@ -180,8 +196,6 @@ def main():
 				launch_end_date = dt.datetime.strptime(launch_end, "%m%d").replace(year=int(dr_yr))
 				# print(launch_start_date)
 				date_range = [launch_start_date + dt.timedelta(days=x) for x in range((launch_end_date - launch_start_date).days + 1)]
-				# manually selecting some dates to investigate, instead of entire date range (defined above)
-				failed_dates = []
 				for date in date_range:
 					# fail_dict[cq][scen][dr_yr][date.strftime("%Y%m%d")] = None
 					run_dir = os.path.join(fee_dir, cq, scen, dr_yr, date.strftime("%Y%m%d"))
@@ -200,11 +214,13 @@ def main():
 					# open file and read the lines at the end of the file
 					with open(full_out_path, 'r') as file:
 						lastlines = list(deque(file, maxlen=last_lines_to_read))
+
 					if lastlines[-1].rstrip('\n') == common_error_msg:
 						print(f"\t\t\t DATE: {date.strftime('%m/%d/%Y')}")
-						print(f"\t\t\t PATH: {run_dir}")
-						error_report = parse_failed_run(lastlines)
-						# failed_dates.append(date.strftime('%Y%m%d'))
+						print(f"\t\t\t PATH: {full_out_path}")
+						error_report = {'error':'run_failure', 'err_type':'unkown', 'file':outfile, 'last_date_ord':-1, 'last_date_dt':-1}
+						# print("\nPASSING TO PARSE_FAILED_RUNS\n")
+						error_report = parse_failed_run(lastlines, error_report)
 						# print(error_report)
 						fail_dict[cq][scen][dr_yr][date.strftime('%Y%m%d')] = error_report
 					# # optional message
@@ -212,7 +228,9 @@ def main():
 
 	# generate the concise report table
 	report = report_df(fail_dict)
-	report.to_csv('err_report.csv')
+	report.to_csv(f'{log_name}.csv')
+
+	print("DEBUG SCRIPT COMPLETE")
 
 if __name__ == '__main__':
 	main()
